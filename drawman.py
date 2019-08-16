f5f6fc84875b91118f2fcfa3e4cc9947a9819c35
User <malevasveta@gmail.com>
2019-08-16 17:57:26 +0300

    дороботки по масштабированию

--- drawman.py
+++ drawman.py
@@ -1,12 +1,16 @@
 from turtle import Turtle
 
 def init_drawman():
-    global t, x_current, y_current
+    global t, x_current, y_current, _drawman_scale
     t = Turtle()
     t.penup()
     x_current = 0
     y_current = 0
     t.goto(x_current, y_current)
+
+def drawman_scale(scale):
+    global _drawman_scale
+    _drawman_scale = scale
 def test_drawman():
     """
     Тестирование работы чертежника
@@ -26,16 +30,13 @@ def pen_up():
     t.penup()
 
 def on_vector(dx, dy):
-    global x_current, y_current
-    x_current += dx
-    y_current += dy
-    t.goto(x_current, y_current)
+    to_point(x_current + dx, y_current + dy)
 
 def to_point(x, y,):
     global x_current, y_current
     x_current = x
     y_current = y
-    t.goto(x_current, y_current)
+    t.goto(_drawman_scale*x_current, _drawman_scale*y_current)
 
 
 init_drawman()
